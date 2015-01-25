43384e7849cb50555b68627249889fdac4978e4d
AnimeWiki <quiloos39@gmail.com>
2015-01-25 22:15:55 +0200

    Create Doge.lua

---
+++ Doge.lua
@@ -0,0 +1,9 @@
+-- She is so kawai isn't she ? :)
+bl = Instance.new("BillboardGui", script.Parent.Head)
+bl.Size = UDim2.new(1,0,1,0)
+bl.AlwaysOnTop = true
+img = Instance.new("ImageLabel", bl)
+img.BackgroundTransparency = 1
+img.Image = "rbxassetid://189376947"
+img.Size = UDim2.new(3,0,3,0)
+img.Position = UDim2.new(-1,0,-1,0)
