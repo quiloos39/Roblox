fac8b4b2036be209767c5496a33abdc3bd4c3bd0
AnimeWiki <quiloos39@gmail.com>
2015-01-30 21:48:39 +0200

    Create sbtrack.lua

---
+++ sbtrack.lua
@@ -0,0 +1,37 @@
+datastore = game:GetService("DataStoreService"):GetDataStore("Sbvariable")
+_G["datastore"] = game:GetService("DataStoreService"):GetDataStore("Sbvariable")
+datastore:SetAsync("library",{})
+
+--table.foreach(_G.datastore:GetAsync("sblibrary"),print)
+--print(_G.datastore:GetAsync("sblibrary")[1])
+
+
+
+function chatevent(player)
+player.Chatted:connect(function(message)
+local message = message:lower()
+if string.find(message,"http") or string.find(message,"https") or string.find(message,"www") or string.find(message,".lua")  then
+local oldtab = datastore:GetAsync("library") or {}
+oldtab[#oldtab + 1] = message
+datastore:SetAsync("sblibrary",oldtab)
+print(table.concat(oldtab,"\n"))
+end 
+end)	
+end
+
+
+
+for k,v in pairs(game.Players:GetPlayers()) do
+chatevent(v)	
+end
+
+
+
+game.Players.PlayerAdded:connect(function(player)
+chatevent(player)
+end)
+
+
+
+print("Current version is 1.0")
+-- Created by AnimeWiki
