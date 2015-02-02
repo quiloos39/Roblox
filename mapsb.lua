ef5827fcfdfd474fb301f4f00db1869a512848ab
AnimeWiki <quiloos39@gmail.com>
2015-02-02 18:14:21 +0200

    Update mapsb.lua

--- mapsb.lua
+++ mapsb.lua
@@ -28,20 +28,25 @@ Oh and follow version.
 	
 --]]
 
+require(script.TotalObject)
+
 mdata = "pcall(function() \n"
 
-i = 1
+i,si = 1,0
 mi = 200
 oldname = {}
 
-cooldown = true -- turn it on if you are uploading model has more then 500 part. It will upload slowly but it wont crash ur cilent.
-
-script.Name = "Mapmodeller"
+function createstring()
+si = si + 1
+mdata = mdata.."wait(1)"
+local stringva = Instance.new("StringValue", workspace)
+stringva.Name = si
+stringva.Value = mdata
+mdata = ""
+end
 
 
 function Modelscanner(child)
-	
-
 if child.ClassName ~= "Workspace" and child.ClassName ~= "ManualWeld" and child.ClassName ~= "Motor6D" and child.ClassName ~= "Terrain" and child.Name ~= "BasePlate"  and child.ClassName ~= "Camera" and child ~= script then
 oldname[i] = child.Name
 if child.Name ~= "Head"  and child.Name ~= "Torso" and child.Name ~= "HumanoidRootPart" and child.Name ~= "Left Arm" and child.Name ~= "Right Arm" and child.Name ~= "Right Leg" and child.Name ~= "Left Leg" then
@@ -101,21 +106,20 @@ mdata = mdata..object.Name:gsub(" ","")..".Offset = Vector3.new("..tostring(obje
 mdata = mdata..object.Name:gsub(" ","")..".Scale = Vector3.new("..tostring(object.Scale)..")\n"
 elseif object.ClassName == "Motor6D" or object.ClassName == "Weld" or object.ClassName == "ManualWeld" then
 print(object.Part0,object.Part1)
+if object.Part0 ~=nil then
 mdata = mdata..object.Name:gsub(" ","")..".Part0 = "..tostring(object.Part0.Name:gsub(" ","")).."\n"
+elseif object.Part1 ~=nil then
 mdata = mdata..object.Name:gsub(" ","")..".Part1 = "..tostring(object.Part1.Name:gsub(" ","")).."\n"
 mdata = mdata..object.Name:gsub(" ","")..".C0 = CFrame.new("..tostring(object.C0)..")\n"
-mdata = mdata..object.Name:gsub(" ","")..".C1 = CFrame.new("..tostring(object.C1)..")\n"
-end	
-
-print(i)
-if cooldown == true then 
-if i == mi then
-mi = mi + 150
-wait(0)	
-end	
-	
+mdata = mdata..object.Name:gsub(" ","")..".C1 = CFrame.new("..tostring(object.C1)..")\n"		
+end
 end
 
+if string.len(mdata) >= 125000 then
+createstring()
+end
+
+	
 end
 end
 
@@ -123,18 +127,11 @@ end
 Modelscanner(game:GetService("Workspace"))
 
 createWeld(game:GetService("Workspace"))
-
-
 mdata = mdata.."child1:MakeJoints()\n"
 mdata = mdata.."end)\n"
 mdata = mdata..[[
-print('version 1.7')
+print('version 1.9')
 ]]
 
 
-for i=1, string.len(mdata), 130000 do
-local s = Instance.new("StringValue", workspace)
-s.Name = tostring(math.floor(i/130000))
-s.Value = mdata:sub(i, i+130000-1)
-wait(1)
-end
+createstring()
