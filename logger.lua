c42950c979181942b1fc4987614866e587c90ba9
AnimeWiki <quiloos39@gmail.com>
2015-02-15 22:14:11 +0200

    Update logger.lua

--- logger.lua
+++ logger.lua
@@ -2,9 +2,10 @@ local _ = {}
 
 function save(v,message)	
 if _[v] ==nil then 	
-table.insert(_,{v = {}})	
+local v = {message}	
+table.insert(_,v)	
 end
-print(_[1][1])
+print(_[1])
 end
 for k,v in pairs(game:GetService("Players"):GetPlayers()) do
 v.Chatted:connect(function(message) save(v.Name,message) end)
