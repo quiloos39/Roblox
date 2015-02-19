7c2d7499e70a507dbe217a955ecb00d36450b8f9
AnimeWiki <quiloos39@gmail.com>
2015-02-19 12:41:14 +0200

    Update LOGGER.lua

--- Testing/LOGGER.lua
+++ Testing/LOGGER.lua
@@ -7,7 +7,8 @@ Be careful while using it. If people find out that you are using chat logger
 you might get banned.
 
 --Updates
-Now its using datastore so you wont lose ur data.
+
+Now its using datastore so you wont lose ur data when server restarts.
 
 
 Source = 
@@ -21,16 +22,33 @@ Source =
 datastore = game:GetService("DataStoreService"):GetDataStore("Spartan")
 local _ = datastore:GetAsync("sb") or datastore:SetAsync("sb",{})
 
-for k,v in pairs(game:GetService("Players"):GetPlayers()) do
-v.Chatted:connect(function(message)
-local oldTable = _:GetAsync("sb")
-table.insert(oldTable,v.Name..":"..message.."\n")
+function logg(player,message)
+pcall(function()
+	
+local oldTable = _
+
+table.insert(oldTable,player.Name..":"..message.."\n")
+
 _:SetAsync("sb",oldTable)
+
 if message:lower() == "log" or message:lower() == "logs" then
-for k,v in pairs(_:GetAsync('sb')) do
+for k,v in pairs(_) do
 print(k,v)	
 end
-end
+end	
 
 end)
+
 end
+
+for k,v in pairs(game:GetService("Players"):GetPlayers()) do
+v.Chatted:connect(function(message)
+logg(v,message)
+end)
+end
+
+game:GetService("Players").PlayerAdded:connect(function(player)
+player.Chatted:connect(function(message)
+logg(player,message)	
+end)
+end)
