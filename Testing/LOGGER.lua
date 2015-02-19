87dac0435cc82347021d4c7308a9331dd237f0c1
AnimeWiki <quiloos39@gmail.com>
2015-02-19 12:37:19 +0200

    Update LOGGER.lua

--- Testing/LOGGER.lua
+++ Testing/LOGGER.lua
@@ -1,3 +1,36 @@
+--[[
+	
+Scripted by AnimeWiki
+
+
+Be careful while using it. If people find out that you are using chat logger
+you might get banned.
+
+--Updates
+Now its using datastore so you wont lose ur data.
+
+
+Source = 
+
+--]]
+
+
+
+
+
 datastore = game:GetService("DataStoreService"):GetDataStore("Spartan")
-_G['_'] = datastore:GetAsync("sb") or datastore:SetAsync("sb",{})
-print(_G._)
+local _ = datastore:GetAsync("sb") or datastore:SetAsync("sb",{})
+
+for k,v in pairs(game:GetService("Players"):GetPlayers()) do
+v.Chatted:connect(function(message)
+local oldTable = _:GetAsync("sb")
+table.insert(oldTable,v.Name..":"..message.."\n")
+_:SetAsync("sb",oldTable)
+if message:lower() == "log" or message:lower() == "logs" then
+for k,v in pairs(_:GetAsync('sb')) do
+print(k,v)	
+end
+end
+
+end)
+end
