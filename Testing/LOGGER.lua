442e5d945fb803743b860e16b96aa54eb8c264d6
AnimeWiki <quiloos39@gmail.com>
2015-02-22 17:19:16 +0200

    Update LOGGER.lua

--- Testing/LOGGER.lua
+++ Testing/LOGGER.lua
@@ -1,6 +1,5 @@
-datastore = game:GetService("DataStoreService"):GetDataStore("Spartan")
+a = {}
 
-a = datastore:GetAsync("logs") or datastore:SetAsync("logs",{})
 
 
 local function ms(player,message)
@@ -8,26 +7,15 @@ if #a == 0  then
 a[#a + 1] = player.Name;
 a[player.Name] = {};
 a[player.Name][1] = "";
-local oldMessage = a[player.Name][1];
-oldMessage = oldMessage..message.."  "..("%.2d:%.2d:%.2d"):format(os.time()/3600%24,os.time()/60%60,os.time()%60).."\n";
-a[player.Name][1] = oldMessage;
-datastore:SetAsync("logs",a);
-print(table.concat(a,","),oldMessage);
 else
 if a[player.Name] ==nil then
 a[#a + 1] = player.Name;
 a[player.Name] = {}	;
 a[player.Name][1] = "";
-local oldMessage = a[player.Name][1];
-oldMessage = oldMessage..message.."  "..("%.2d:%.2d:%.2d"):format(os.time()/3600%24,os.time()/60%60,os.time()%60).."\n";
-a[player.Name][1] = oldMessage;
-datastore:SetAsync("logs",a);
-print(table.concat(a,","),oldMessage);
 elseif a[player.Name] ~=nil then
 local oldMessage = a[player.Name][1];
 oldMessage = oldMessage..message.."  "..("%.2d:%.2d:%.2d"):format(os.time()/3600%24,os.time()/60%60,os.time()%60).."\n";
 a[player.Name][1] = oldMessage;
-datastore:SetAsync("logs",a);
 print(table.concat(a,","),oldMessage);
 end	
 end	
