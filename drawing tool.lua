3e5bbfd408da1113d1cb80aa74cfe091e5d139b0
AnimeWiki <quiloos39@gmail.com>
2015-02-23 23:28:48 +0200

    Update drawing tool.lua

--- drawing tool.lua
+++ drawing tool.lua
@@ -2,6 +2,13 @@ player = game.Players.LocalPlayer
 mouse = player:GetMouse()
 
 a = false
+color = "White"
+
+player.Chatted:connect(function(message)
+if message:sub(1,5) == "color" then
+color = message:sub(6)
+end
+end)
 
 mouse.Button1Down:connect(function()
 local y = mouse.Hit.Y
@@ -11,6 +18,7 @@ local par = Instance.new("Part", player.Character)
 par.Name = "line"
 par.FormFactor = "Custom"
 par.Anchored = true 
+part.BrickColor = BrickColor.new(color)
 par.BrickColor = BrickColor.White() -- problem starts here..
 par.Size = Vector3.new(1,.1,1)  -- it doesnt read whole properties.
 par.CFrame = CFrame.new(mouse.Hit.X,y,mouse.Hit.Z)  -- does it matter ._. so it wont show rotate
