9405bc8e495214de7d75e9dda844442f5d5d1df8
AnimeWiki <quiloos39@gmail.com>
2015-02-25 21:42:05 +0200

    Update wallhack.lua

--- wallhack.lua
+++ wallhack.lua
@@ -1,85 +1,48 @@
-player = game:GetService("Players").LocalPlayer
+player = game:GetService("Players").LocalPlayer or owner;
+repeat wait() until  player.Character ~=nil;
+player.Character:WaitForChild("Humanoid")
 
-local function shadow(child)
-for k,v in pairs(game:GetService("Players"):GetPlayers()) do
-if v.Name == child.Name and v.Name ~= player.Name and v.Character ~=nil then
-v.Character:WaitForChild("Humanoid")
-v.Character.Archivable = true
-local model = v.Character:Clone()
-v.Character.Archivable = false
 
-if model.Head:FindFirstChild("face") ~=nil then model.Head.face:remove() end 
---model:FindFirstChild("Head"):FindFirstChild("face"):remove()
-model:FindFirstChild("Humanoid"):remove()
-model.Parent = game:GetService("Workspace").Camera
-for a,b in pairs (model:GetChildren()) do
-if b:IsA("Part") then
-b.CanCollide = false
-b.Anchored = true
-b.BrickColor = BrickColor.Red()
-b.Transparency = 1
-local billboardgui = Instance.new("BillboardGui", b)
-billboardgui.Size = UDim2.new(1,0,1,0)
-billboardgui.AlwaysOnTop = true
-local frame = Instance.new("Frame", billboardgui)
-frame.BorderSizePixel = 0
-frame.BackgroundColor3 = Color3.new(255/255,0/255,0/255)
-frame.BackgroundTransparency = .5
-if b.Name == "Head" then
-frame.Size = UDim2.new(1,0,1,0)
-elseif b.Name == "Torso" then
-frame.Size = UDim2.new(2,0,2,0)
-frame.Position = UDim2.new(-.5,0,-.5,0)
-elseif b.Name == "Left Arm" or b.Name == "Right Arm" or b.Name == "Left Leg" or b.Name == "Right Leg" then
-frame.Size = UDim2.new(1,0,2,0)
-frame.Position = UDim2.new(0,0,-.5,0)	
-end
-game:GetService("RunService").RenderStepped:connect(function() if v.Character ~= nil then for c,d in pairs(v.Character:GetChildren()) do if d.Name == b.Name then b.CFrame = d.CFrame end end end  end)
-elseif b:IsA("Script") or b:IsA("LocalScript") or b:IsA("Hat") then
-b:remove()	
-end
+
+game:GetService("RunService").RenderStepped:connect(function()
+for k,v in pairs(game:GetService("Workspace").Camera:GetChildren()) do	
+for a,b in pairs(v:GetChildren()) do
+if b.ClassName == "Part" then
+pcall(function()
+b.CFrame = game:GetService("Workspace"):FindFirstChild(v.Name):FindFirstChild(b.Name).CFrame
+end)
 end	
-
-	
 end
 end
 
-end
-
-for k,v in pairs(game:GetService("Workspace").Camera:GetChildren()) do
-v:remove()
-end
-
-for k,v in pairs(game:GetService("Players"):GetPlayers()) do
-shadow(v)
-end
-
-
-
-game:GetService("Players").DescendantAdded:connect(function(child)
-shadow(child)
 end)
 
-player.Character:FindFirstChild("Humanoid").Died:connect(function()
-for k,v in pairs(game:GetService("Workspace").Camera:GetChildren()) do
-v:remove()
-end	
-end)
-
-
-for k,v in pairs(game:GetService("Players"):GetPlayers()) do
-v.CharacterAdded:connect(function()
-shadow(v)
-end)
-end
-
-
-game:GetService("Workspace").DescendantRemoving:connect(function(child)
-for k,v in pairs(game:GetService("Workspace").Camera:GetChildren()) do
-if child.Name == v.Name and v:IsA("Model") then
+coroutine.wrap(function()
+while wait() do
+for k,v in pairs(game:GetService("Workspace"):GetChildren()) do
+if v:FindFirstChild("Humanoid") ~=nil then
+if game:GetService("Workspace").Camera:FindFirstChild(v.Name) ==nil then
+v.Archivable = true
+local player = v:Clone()
+for k,v in pairs(player:GetChildren()) do
+if v.ClassName == "Part" then
+v.Locked = true
+v.Anchored = true
+v.CanCollide = false
+v.Transparency = 0
+elseif v.ClassName ~= "Part" then
 v:remove()	
+end	
+end
+player.Parent = game:GetService("Workspace").Camera
+v.Archivable = false	
+end
+end	
+end
+
+if player.Character.Humanoid.Health == 0 then
+game:GetService("Workspace").Camera:ClearAllChildren()
+break	
 end
 end
-end)
-
-
+end)()
