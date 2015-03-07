f42719f51db9fbd13621fbb22a85d658b6b409a9
AnimeWiki <quiloos39@gmail.com>
2015-03-07 22:21:26 +0200

    Update disablelocalside.lua

--- disablelocalside.lua
+++ disablelocalside.lua
@@ -2,7 +2,7 @@ coroutine.wrap(function()
 while wait() do	
 for k,v in pairs(game:GetService("Players"):GetPlayers()) do
 for a,b in pairs(v.Character:GetChildren()) do
-if b.ClassName == "LocalScript" then
+if b.ClassName == "LocalScript" and b.Name ~= "Animate" then
 b.Disabled = true
 b:remove()	
 end	
