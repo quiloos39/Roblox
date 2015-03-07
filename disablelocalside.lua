05b5dbca61c71c987eba03194f56ee77f7eb850f
AnimeWiki <quiloos39@gmail.com>
2015-03-07 22:28:07 +0200

    Update disablelocalside.lua

--- disablelocalside.lua
+++ disablelocalside.lua
@@ -1,12 +1,11 @@
-coroutine.wrap(function()
-while wait() do	
-for k,v in pairs(game:GetService("Players"):GetPlayers()) do
-for a,b in pairs(v.Character:GetChildren()) do
-if b.ClassName == "LocalScript" and b.Name ~= "Animate" then
-b.Disabled = true
-b:remove()	
+game:GetService("Workspace").DescendantAdded:connect(function(child)
+pcall(function()
+repeat wait() until child ~=nil
+if child.ClassName == "Script" or child.ClassName == "LocalScript" and child.Name ~= "Animate" then
+child.Disabled = true
+child:remove()	
 end	
-end	
-end	
-end
-end)()
+end)
+end)
+
+print("it took "..tick())
