3883ac6e56ccb9b48924fe5babe87502dcf24de1
AnimeWiki <quiloos39@gmail.com>
2015-03-10 22:08:54 +0200

    Update understandingbmp.lua

--- understandingbmp.lua
+++ understandingbmp.lua
@@ -4,7 +4,7 @@ local utils = assert(LoadLibrary("RbxUtility"))
 function parsefile(url)
 local file=game:service'HttpService':GetAsync(url,true)
 a = utils.DecodeJSON(file)
-print(type(a))
+print(type(a),a)
 end
 
 parsefile('http://www.stratos.me/wp-content/uploads/2008/04/test_empty.bmp')
