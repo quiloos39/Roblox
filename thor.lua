c7df0a05b74b30e6c8c61f050a0328a3a1562f78
AnimeWiki <quiloos39@gmail.com>
2015-04-18 18:01:14 +0300

    Update thor.lua

--- thor.lua
+++ thor.lua
@@ -1,21 +1,17 @@
---bob
+firstarg = ":"
+
+com = {
+"kill",
+"kick",
+"ban"	
+}
 
 
-firstargument = ":"
-
-commands = {"kill"}
 
 function message(message)
-if string.sub(message,1,1) == firstargument then
-for k,v in pairs(commands) do
-if message:lower() == string.sub(v:lower(),1,string.len(message)) then
-print(k,v)	
-end
-end
-
-end
+print(message)
 end
 
 
 
-game:GetService("Players").AnimeWiki.Chatted:connect(message)
+player = game:GetService("Players"):FindFirstChild("AnimeWiki") or game:GetService("Players").LocalPlayer
