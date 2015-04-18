4aa5fc2d1b980dcf7c9fea555c5bf11d51d63bca
AnimeWiki <quiloos39@gmail.com>
2015-04-18 17:51:15 +0300

    Update thor.lua

--- thor.lua
+++ thor.lua
@@ -1,7 +1,16 @@
 firstargument = ":"
 
+commands = {"kill"}
+
 function message(message)
-print(message)
+if string.sub(message,1,1) == firstargument then
+for k,v in pairs(commands) do
+if message:lower() == string.sub(v:lower(),1,string.len(message)) then
+print(k,v)	
+end
+end
+
+end
 end
 
 
