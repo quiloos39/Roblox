7f08895ca686d1f6cb12851f2cb091f0f26a73a0
AnimeWiki <quiloos39@gmail.com>
2015-04-18 20:36:45 +0300

    Update thor.lua

--- thor.lua
+++ thor.lua
@@ -1,37 +1,47 @@
-local firstarg = ":"
-local comma = {"kill","kick","ban"}
-
 local player = game:GetService("Players").LocalPlayer or game:GetService("Players"):FindFirstChild("AnimeWiki")
 
+
+local comma = {"kill"}
 local function runf(func,arg)
-print(func,arg)	
-if func == "kill" and arg ~=nil then
-for k,v in pairs(game:GetService("Players"):GetPlayers()) do
-if arg:lower() == string.sub(v.Name:lower(),1,string.len(arg)) then
-print(v.Name)
-pcall(function() v.Character:FindFirstChild("Humanoid"):TakeDamage(v.Character:FindFirstChild("Humanoid").MaxHeatlh) end)
+print(func,arg[2])
+if func == "kill" then
+if arg ~=nil then
+
+else
+player.Character:FindFirstChild("Humanoid"):TakeDamage(player.Character:FindFirstChild("Humanoid").MaxHealth)
 end
+end
+end
+
+local function message(message)
+if string.sub(message,1,1) == ":" then
+local player = {}
+local co = {}
+local func = "" pcall(function() if string.find(message, " ") ~=nil then func = string.sub(message,1,string.find(message," ") - 1) else func = string.sub(message,2,string.len(message))  end  end) 
+local arg = "" pcall(function() arg = string.sub(message,string.find(message," ") + 1,string.len(message)) end) 
+for k,v in pairs(comma)  do
+if string.sub(v:lower(),1,string.len(func)) == func:lower() then
+co[#co + 1] = v
 end	
-elseif func == "kick" then
+end
+if string.len(arg) > 0 then
+for com in string.gmatch(arg,",") do
+player[#player + 1] = string.sub(arg,1,string.find(arg,",") - 1)
+arg = string.sub(arg,string.find(arg,",") + 1,string.len(arg))
+end
+player[#player + 1] = arg	
+end
+
+if #co >= 2 then
+
+else
+runf(func,player)	
+end
 	
 end
 end
 
 
-local function message(message)
-local cho = {}	
-local arg pcall(function() arg = string.sub(message,string.find(message," ") + 1,string.len(message)) end)	
-local func = string.sub(message,1,string.find(message," ")):gsub(" ","")
-for k,v in pairs(comma) do
-if func:lower() == string.sub(v:lower(),1,string.len(func)) then
-cho[#cho + 1] = v
-end
-end
-if #cho == 1 then
-runf(cho[1],arg)	
-end
-print(func,arg)
-end
-
-
 player.Chatted:connect(message)
+--message(":kill an,bob")
+--message(":ki an,bob")
