6bc7abaaf694fc21d2141ae1fe35b1d138be6f0f
AnimeWiki <quiloos39@gmail.com>
2015-05-02 20:21:41 +0300

    Update luapad.lua

--- luapad.lua
+++ luapad.lua
@@ -1,63 +1,48 @@
-
-datastore = game:GetService("DataStoreService"):GetDataStore("SE")
-
-player = owner or game:GetService("Players"):FindFirstChild("AnimeWiki") or game:GetService("Players"):FindFirstChild("Player")
-if datastore:GetAsync(player.Name) == nil then
-oldat = datastore:SetAsync(player.Name,{tabs = {},current = "print('Hello world!')"})	
-elseif datastore:GetAsync(player.Name) ~= nil then
-oldat = datastore:GetAsync(player.Name)	
-end
-
-repeat wait() until player.Character ~=nil
-player.Character:WaitForChild("Humanoid")
-
-
-q1 = Instance.new('ScreenGui',player.PlayerGui)
+local q1 = Instance.new('ScreenGui', owner:FindFirstChild("PlayerGui"))
 q1.Name = 'ScreenGui'
 q1.Archivable = true
-q2 = Instance.new('Frame',q1)
+local q2 = Instance.new('Frame',q1)
 q2.BackgroundColor3 = Color3.new(1, 1, 1)
 q2.BackgroundTransparency = 0
 q2.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
 q2.BorderSizePixel = 1
 q2.Name = 'Frame'
-q2.Position = UDim2.new(0,98,0,54)
+q2.Position = UDim2.new(0,0,0.60000002384186,0)
 q2.Rotation = 0
-q2.Size = UDim2.new(0.5,0,0.5,0)
+q2.Size = UDim2.new(1,0,0.40000000596046,0)
 q2.SizeConstraint = Enum.SizeConstraint.RelativeXY
 q2.Style = Enum.FrameStyle.Custom
 q2.Visible = true
 q2.ZIndex = 1
 q2.Archivable = true
 q2.ClipsDescendants = false
-q2.Draggable = true
-q2.Active = true
-q3 = Instance.new('Frame',q2)
+q2.Draggable = false
+q2.Active = false
+local q3 = Instance.new('ScrollingFrame',q2)
 q3.BackgroundColor3 = Color3.new(1, 1, 1)
 q3.BackgroundTransparency = 0
-q3.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+q3.BorderColor3 = Color3.new(1, 1, 1)
 q3.BorderSizePixel = 1
-q3.Name = 'Frame'
-q3.Position = UDim2.new(0,0,0.050000000745058,0)
+q3.Name = 'ScrollingFrame'
+q3.Position = UDim2.new(0,0,0.070000000298023,0)
 q3.Rotation = 0
-q3.Size = UDim2.new(1,0,0.56999999284744,0)
+q3.Size = UDim2.new(1,0,0.93000000715256,0)
 q3.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q3.Style = Enum.FrameStyle.Custom
 q3.Visible = true
 q3.ZIndex = 1
 q3.Archivable = true
 q3.ClipsDescendants = true
 q3.Draggable = false
 q3.Active = false
-q4 = Instance.new('Frame',q3)
+local q4 = Instance.new('Frame',q3)
 q4.BackgroundColor3 = Color3.new(0.866667, 0.866667, 0.866667)
 q4.BackgroundTransparency = 0
 q4.BorderColor3 = Color3.new(0.866667, 0.866667, 0.866667)
-q4.BorderSizePixel = 1
+q4.BorderSizePixel = 0
 q4.Name = 'Frame'
 q4.Position = UDim2.new(0,0,0,0)
 q4.Rotation = 0
-q4.Size = UDim2.new(0.013000000268221,0,1,0)
+q4.Size = UDim2.new(0.025000000372529,0,1,0)
 q4.SizeConstraint = Enum.SizeConstraint.RelativeXY
 q4.Style = Enum.FrameStyle.Custom
 q4.Visible = true
@@ -66,15 +51,15 @@ q4.Archivable = true
 q4.ClipsDescendants = false
 q4.Draggable = false
 q4.Active = false
-q5 = Instance.new('Frame',q3)
+local q5 = Instance.new('Frame',q3)
 q5.BackgroundColor3 = Color3.new(0.905882, 0.905882, 0.905882)
 q5.BackgroundTransparency = 0
 q5.BorderColor3 = Color3.new(0.905882, 0.905882, 0.905882)
-q5.BorderSizePixel = 1
+q5.BorderSizePixel = 0
 q5.Name = 'Frame'
-q5.Position = UDim2.new(0.013000000268221,0,0,0)
+q5.Position = UDim2.new(0.025000000372529,0,0,0)
 q5.Rotation = 0
-q5.Size = UDim2.new(0.013000000268221,0,1,0)
+q5.Size = UDim2.new(0.021999999880791,0,1,0)
 q5.SizeConstraint = Enum.SizeConstraint.RelativeXY
 q5.Style = Enum.FrameStyle.Custom
 q5.Visible = true
@@ -83,301 +68,174 @@ q5.Archivable = true
 q5.ClipsDescendants = false
 q5.Draggable = false
 q5.Active = false
-q6 = Instance.new('Frame',q3)
-q6.BackgroundColor3 = Color3.new(0.980392, 0.980392, 0.980392)
+local q6 = Instance.new('TextBox',q3)
+q6.BackgroundColor3 = Color3.new(1, 1, 1)
 q6.BackgroundTransparency = 0
-q6.BorderColor3 = Color3.new(0.980392, 0.980392, 0.980392)
+q6.BorderColor3 = Color3.new(1, 1, 1)
 q6.BorderSizePixel = 1
-q6.Name = 'Frame'
-q6.Position = UDim2.new(0.026000000536442,0,0,0)
+q6.Name = 'TextBox'
+q6.Position = UDim2.new(0.068999998271465,0,0,0)
 q6.Rotation = 0
-q6.Size = UDim2.new(0.0099999997764826,0,1,0)
+q6.Size = UDim2.new(0.88999998569489,0,1,0)
 q6.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q6.Style = Enum.FrameStyle.Custom
 q6.Visible = true
 q6.ZIndex = 1
 q6.Archivable = true
 q6.ClipsDescendants = false
 q6.Draggable = false
-q6.Active = false
-q7 = Instance.new('Frame',q3)
-q7.BackgroundColor3 = Color3.new(1, 1, 1)
-q7.BackgroundTransparency = 0
-q7.BorderColor3 = Color3.new(1, 1, 1)
-q7.BorderSizePixel = 1
-q7.Name = 'Frame'
-q7.Position = UDim2.new(0.035999998450279,0,0,0)
-q7.Rotation = 0
-q7.Size = UDim2.new(0.96399998664856,0,1,0)
-q7.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q7.Style = Enum.FrameStyle.Custom
-q7.Visible = true
-q7.ZIndex = 1
-q7.Archivable = true
-q7.ClipsDescendants = true
-q7.Draggable = false
-q7.Active = false
-q8 = Instance.new('ScrollingFrame',q7)
-q8.BackgroundColor3 = Color3.new(1, 1, 1)
+q6.ClearTextOnFocus = false
+q6.MultiLine = true
+q6.Font = Enum.Font.SourceSans
+q6.FontSize = Enum.FontSize.Size18
+q6.Text = 'print("Hello World.!")'
+q6.TextColor3 = Color3.new(0, 0, 0)
+q6.TextScaled = false
+q6.TextStrokeColor3 = Color3.new(0, 0, 0)
+q6.TextStrokeTransparency = 1
+q6.TextTransparency = 0
+q6.TextWrapped = true
+q6.TextXAlignment = Enum.TextXAlignment.Left
+q6.TextYAlignment = Enum.TextYAlignment.Top
+q6.Active = true
+local q8 = Instance.new('Frame',q3)
+q8.BackgroundColor3 = Color3.new(0.980392, 0.980392, 0.980392)
 q8.BackgroundTransparency = 0
-q8.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q8.BorderSizePixel = 1
-q8.Name = 'ScrollingFrame'
-q8.Position = UDim2.new(0,0,0,0)
+q8.BorderColor3 = Color3.new(0.980392, 0.980392, 0.980392)
+q8.BorderSizePixel = 0
+q8.Name = 'Frame'
+q8.Position = UDim2.new(0.046999998390675,0,0,0)
 q8.Rotation = 0
-q8.Size = UDim2.new(1,0,1,0)
+q8.Size = UDim2.new(0.021999999880791,0,1,0)
 q8.SizeConstraint = Enum.SizeConstraint.RelativeXY
+q8.Style = Enum.FrameStyle.Custom
 q8.Visible = true
 q8.ZIndex = 1
 q8.Archivable = true
-q8.ClipsDescendants = true
+q8.ClipsDescendants = false
 q8.Draggable = false
-q8.Active = true
-q9 = Instance.new('TextBox',q8)
-q9.BackgroundColor3 = Color3.new(1, 1, 1)
-q9.BackgroundTransparency = 1
-q9.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q9.BorderSizePixel = 0
-q9.Name = 'Source'
+q8.Active = false
+local q9 = Instance.new('Frame',q2)
+q9.BackgroundColor3 = Color3.new(0.470588, 0.470588, 0.470588)
+q9.BackgroundTransparency = 0
+q9.BorderColor3 = Color3.new(0.25098, 0.25098, 0.25098)
+q9.BorderSizePixel = 1
+q9.Name = 'Frame'
 q9.Position = UDim2.new(0,0,0,0)
 q9.Rotation = 0
-q9.Size = UDim2.new(0.97000002861023,0,1,0)
+q9.Size = UDim2.new(1,0,0.070000000298023,0)
 q9.SizeConstraint = Enum.SizeConstraint.RelativeXY
+q9.Style = Enum.FrameStyle.Custom
 q9.Visible = true
-q9.ZIndex = 1
+q9.ZIndex = 2
 q9.Archivable = true
-q9.ClipsDescendants = false
+q9.ClipsDescendants = true
 q9.Draggable = false
-q9.ClearTextOnFocus = false
-q9.MultiLine = true
-q9.Font = Enum.Font.ArialBold
-q9.FontSize = Enum.FontSize.Size14
-q9.Text = oldat["current"]
-q9.TextColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q9.TextScaled = false
-q9.TextStrokeColor3 = Color3.new(0, 0, 0)
-q9.TextStrokeTransparency = 1
-q9.TextTransparency = 0
-q9.TextWrapped = true
-q9.TextXAlignment = Enum.TextXAlignment.Left
-q9.TextYAlignment = Enum.TextYAlignment.Top
-q9.Active = true
-q10 = Instance.new('Script',q9)
-q10.Name = 'Script'
+q9.Active = false
+local q10 = Instance.new('TextButton',q9)
+q10.BackgroundColor3 = Color3.new(0, 0.501961, 0)
+q10.BackgroundTransparency = 0
+q10.BorderColor3 = Color3.new(0.768627, 0.768627, 0.768627)
+q10.BorderSizePixel = 0
+q10.Name = 'TextButton'
+q10.Position = UDim2.new(0.80000001192093,0,0,0)
+q10.Rotation = 0
+q10.Size = UDim2.new(0.20000000298023,0,1,0)
+q10.SizeConstraint = Enum.SizeConstraint.RelativeXY
+q10.Style = Enum.ButtonStyle.Custom
+q10.Visible = true
+q10.ZIndex = 3
 q10.Archivable = true
-q11 = Instance.new('Frame',q2)
-q11.BackgroundColor3 = Color3.new(0.470588, 0.470588, 0.470588)
-q11.BackgroundTransparency = 0
-q11.BorderColor3 = Color3.new(0.25098, 0.25098, 0.25098)
-q11.BorderSizePixel = 1
-q11.Name = 'Frame'
-q11.Position = UDim2.new(0,0,0,0)
-q11.Rotation = 0
-q11.Size = UDim2.new(1,0,0.050000000745058,0)
-q11.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q11.Style = Enum.FrameStyle.Custom
-q11.Visible = true
-q11.ZIndex = 1
-q11.Archivable = true
-q11.ClipsDescendants = false
-q11.Draggable = false
-q11.Active = false
-q12 = Instance.new('TextButton',q11)
-q12.BackgroundColor3 = Color3.new(0.243137, 0.690196, 0.227451)
-q12.BackgroundTransparency = 0
-q12.BorderColor3 = Color3.new(0.243137, 0.690196, 0.227451)
-q12.BorderSizePixel = 1
-q12.Name = 'RUN'
-q12.Position = UDim2.new(0.89999997615814,0,0,0)
-q12.Rotation = 0
-q12.Size = UDim2.new(0.10000000149012,0,1,0)
-q12.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q12.Style = Enum.ButtonStyle.Custom
-q12.Visible = true
-q12.ZIndex = 1
-q12.Archivable = true
-q12.ClipsDescendants = false
-q12.Draggable = false
-q12.Font = Enum.Font.ArialBold
-q12.FontSize = Enum.FontSize.Size12
-q12.Text = 'RUN'
-q12.TextColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q12.TextScaled = false
-q12.TextStrokeColor3 = Color3.new(0, 0, 0)
-q12.TextStrokeTransparency = 1
-q12.TextTransparency = 0
-q12.TextWrapped = false
-q12.TextXAlignment = Enum.TextXAlignment.Center
-q12.TextYAlignment = Enum.TextYAlignment.Center
-q12.AutoButtonColor = true
-q12.Active = true
-q12.Modal = false
-q12.Selected = false
-q13 = Instance.new('Frame',q2)
-q13.BackgroundColor3 = Color3.new(1, 1, 1)
-q13.BackgroundTransparency = 0
-q13.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q13.BorderSizePixel = 1
-q13.Name = 'Frame'
-q13.Position = UDim2.new(0,0,0.62000000476837,0)
-q13.Rotation = 0
-q13.Size = UDim2.new(1,0,0.40000000596046,0)
-q13.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q13.Style = Enum.FrameStyle.Custom
-q13.Visible = true
-q13.ZIndex = 1
-q13.Archivable = true
-q13.ClipsDescendants = true
-q13.Draggable = false
-q13.Active = false
-q14 = Instance.new('TextLabel',q13)
-q14.BackgroundColor3 = Color3.new(0.815686, 0.815686, 0.815686)
-q14.BackgroundTransparency = 0
-q14.BorderColor3 = Color3.new(0.556863, 0.556863, 0.556863)
-q14.BorderSizePixel = 1
-q14.Name = 'TextLabel'
-q14.Position = UDim2.new(0,0,0,0)
-q14.Rotation = 0
-q14.Size = UDim2.new(1,0,0.10000000149012,0)
-q14.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q14.Visible = true
-q14.ZIndex = 1
-q14.Archivable = true
-q14.ClipsDescendants = false
-q14.Draggable = false
-q14.Font = Enum.Font.ArialBold
-q14.FontSize = Enum.FontSize.Size12
-q14.Text = 'Output'
-q14.TextColor3 = Color3.new(0, 0, 0)
-q14.TextScaled = false
-q14.TextStrokeColor3 = Color3.new(0, 0, 0)
-q14.TextStrokeTransparency = 1
-q14.TextTransparency = 0
-q14.TextWrapped = true
-q14.TextXAlignment = Enum.TextXAlignment.Center
-q14.TextYAlignment = Enum.TextYAlignment.Center
-q14.Active = false
-q15 = Instance.new('ScrollingFrame',q13)
-q15.BackgroundColor3 = Color3.new(1, 1, 1)
-q15.BackgroundTransparency = 0
-q15.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q15.BorderSizePixel = 1
-q15.Name = 'ScrollingFrame'
-q15.Position = UDim2.new(0,0,0.10000000149012,0)
-q15.Rotation = 0
-q15.Size = UDim2.new(1,0,0.89999997615814,0)
-q15.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q15.Visible = true
-q15.ZIndex = 1
-q15.Archivable = true
-q15.ClipsDescendants = true
-q15.Draggable = false
-q15.Active = false
-q16 = Instance.new('Frame',q2)
-q16.Name = "Playerlist"
-q16.BackgroundColor3 = Color3.new(1, 1, 1)
-q16.BackgroundTransparency = 0
-q16.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q16.BorderSizePixel = 1
-q16.Name = 'Frame'
-q16.Position = UDim2.new(1,0,0,0)
-q16.Rotation = 0
-q16.Size = UDim2.new(0.30000001192093,0,1.0199999809265,0)
-q16.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q16.Style = Enum.FrameStyle.Custom
-q16.Visible = true
-q16.ZIndex = 1
-q16.Archivable = true
-q16.ClipsDescendants = false
-q16.Draggable = false
-q16.Active = false
-q17 = Instance.new('ScrollingFrame',q16)
-q17.BackgroundColor3 = Color3.new(1, 1, 1)
-q17.BackgroundTransparency = 0
-q17.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q17.BorderSizePixel = 1
-q17.Name = 'ScrollingFrame'
-q17.Position = UDim2.new(0,0,0,0)
-q17.Rotation = 0
-q17.Size = UDim2.new(1,0,1,0)
-q17.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q17.Visible = true
-q17.ZIndex = 1
-q17.Archivable = true
-q17.ClipsDescendants = true
-q17.Draggable = false
-q17.Active = false
+q10.ClipsDescendants = true
+q10.Draggable = false
+q10.Font = Enum.Font.SourceSans
+q10.FontSize = Enum.FontSize.Size14
+q10.Text = 'RUN'
+q10.TextColor3 = Color3.new(1, 1, 1)
+q10.TextScaled = false
+q10.TextStrokeColor3 = Color3.new(0, 0, 0)
+q10.TextStrokeTransparency = 1
+q10.TextTransparency = 0
+q10.TextWrapped = false
+q10.TextXAlignment = Enum.TextXAlignment.Center
+q10.TextYAlignment = Enum.TextYAlignment.Center
+q10.AutoButtonColor = true
+q10.Active = true
+q10.Modal = false
+q10.Selected = false
+local q11 = Instance.new("ScrollingFrame", q2)
+q11.BackgroundTransparency = 11
+q11.BorderSizePixel = 0
+q11.Position = UDim2.new(0,0,0.6,0)
+q11.Size = UDim2.new(0.983,0,1,0)
+q11.Name = "q11"
+q11.ZIndex = 2
+q11.CanvasSize = UDim2.new(0,0,1,0)
+q11.ScrollBarThickness = 0
+
+local a = {}
+a.lines={}
+a.out = {}
 
 
-
-
-local y = 0
-local group = {player.Name}
-local players = {}
-
-local function addline(txt)
-local q16 = Instance.new('TextLabel',q15)
-q16.BackgroundColor3 = Color3.new(1, 1, 1)
-q16.BackgroundTransparency = 1
-q16.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q16.BorderSizePixel = 0
-q16.Name = 'Output'..y
-q16.Position = UDim2.new(0,0,0,y*20)
-q16.Rotation = 0
-q16.Size = UDim2.new(1,0,0,20)
-q16.SizeConstraint = Enum.SizeConstraint.RelativeXY
-q16.Visible = true
-q16.ZIndex = 1
-q16.Archivable = true
-q16.ClipsDescendants = false
-q16.Draggable = false
-q16.Font = Enum.Font.ArialBold
-q16.FontSize = Enum.FontSize.Size14
-q16.Text = txt
-q16.TextColor3 = Color3.new(0.105882, 0.164706, 0.207843)
-q16.TextScaled = false
-q16.TextStrokeColor3 = Color3.new(0, 0, 0)
-q16.TextStrokeTransparency = 1
-q16.TextTransparency = 0
-q16.TextWrapped = true
-q16.TextXAlignment = Enum.TextXAlignment.Left
-q16.TextYAlignment = Enum.TextYAlignment.Center
-q16.Active = false
-y= y + .7
-end
-
-function autosave(ex)
-if ex == true or q9.Parent ~=nil then	
-local success, message = pcall(function()
-oldat["current"] = q9.Text	
-datastore:SetAsync(player.Name,oldat)
-end)
-if not success then
-return autosave()	
+function update()
+--count returns
+local space=1
+for i=1,#q6.Text do
+local c=q6.Text:sub(i,i)
+if string.byte(c)==10 then
+space=space+1
 end
 end
-end		
-
-
-script.Changed:connect(function(val)
-if val == "Parent" then
-autosave(true)
-script:remove()
-elseif val == "Text" and #group > 1 then
-for k,v in pairs(group) do
-if v ~=nil and v.Name ~= player.Name then
-v:FindFirstChild("PlayerGui").ScreenGui.Name = "bob"
-end	
-end	
-end	
-end)
-
-
-coroutine.wrap(function()
-while wait(10) do
-autosave()
+local add=false
+for i=1,space do
+--add new line
+if not a.lines[i] then
+local q11 = Instance.new("TextLabel",q3)
+q11.Size = UDim2.new(0.025,0,0,18)
+q11.FontSize = "Size18"
+q11.Font = "SourceSans"
+q11.Text = i
+q11.BackgroundTransparency = 1
+q11.Position=UDim2.new(0,0,0,q11.Size.Y.Offset*(i-1))
+a.lines[i]=q11
+add=true --here
+-- i have idea but idk it might be buggy.
+-- to make multiplayer support. lol why its going to be like stypi but we need invatation menu or something we need to dedice how its gonna work -- -_- ok
+-- i can make empty box and if u type code there it will show ur friend script ? 
 end
-end)()
+--remove extra lines
+for i=1,#a.lines do
+if i>space then
+if a.lines[i] then a.lines[i]:Destroy()end -- remove e.e remove lag e.e e.e .e. e.e .e.e.e.e.e
+a.lines[i]=nil 
+end
+end
+end
+
+q3.CanvasSize=UDim2.new(0,0,0,18*space)
+if add==true then
+q3.CanvasPosition=Vector2.new(0,18*space) -- thats to make it scroll down if a person types down the page
+-- where is add bool changes ?
+end
+
+end
+
+
+function output(v)
+local out = Instance.new("TextLabel",q11)
+out.BackgroundTransparency = 1
+out.BorderSizePixel = 0
+out.Size = UDim2.new(1,0,0,20)
+out.Position = UDim2.new(0,0,0,#a.out*20)
+out.Text = v
+out.Font = "SourceSans"
+out.FontSize = "Size18"
+out.TextColor3 = Color3.new(195/255,1/255,1/255)
+out.TextWrapped = true
+out.TextXAlignment = Enum.TextXAlignment.Left
+out.TextYAlignment = Enum.TextYAlignment.Center
+a.out[#a.out + 1] = v
+end
 
 
 
@@ -391,25 +249,28 @@ function funcs.print(...)
 	local res = ""
 	for i=1,select("#",...) do
 		res = res.." "..tostring(select(i,...))	
-	end addline(res:sub(2))
+	end output(res:sub(2))
 end
 
---[[
-	I've set the 
-]]
+
 
 local env = setmetatable({},{__index=funcs})
--- is it normal that i am not understending what ur doing right now..
--- let say i typed print'hello world' will it work ? I'll add that
-q12.MouseButton1Click:connect(function()
-	-- loadstring returns 'false, ERROR' when there is a parsing error
-	local suc,err = loadstring(q9.Text)
+
+
+q6.Changed:connect(function(v) if v == "Text" then  update()  end end) -- doesnt fire
+q10.MouseButton1Click:connect(function()
+local suc,err = loadstring(q6.Text)
 	if not suc then -- so print it and return if there is parsing error
-		return addline("Parsing error: "..err)	
+		return output("Parsing error: "..err)	
 	end setfenv(suc,env) -- set environment to 'env'
 	suc,err = pcall(suc) -- run it in pcall to get error
 	if not suc then -- if error during running, print it
-		addline("Running error: "..err)	
+		output("Running error: "..err)	
 	end
-end)
+	
+	
+end) -- i forget ()    
+
+
+
 
