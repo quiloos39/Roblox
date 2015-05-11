79f2035b5668a2bc663f46a5067e5050b5bbda64
AnimeWiki <quiloos39@gmail.com>
2015-05-11 21:33:47 +0300

    Update Keypad.lua

--- Keypad.lua
+++ Keypad.lua
@@ -12,6 +12,9 @@ require(workspace.Module)("12")
 
 ]]
 
+letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",0,1,2,3,4,5,6,7,8,9}
+
+
 return function(v)
 local ti = tick()
 local library = {}
