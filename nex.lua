af5e7e5da6e001d082330ae39aa11e092089d651
AnimeWiki <quiloos39@gmail.com>
2015-05-19 16:33:54 +0300

    Update nex.lua

--- nex.lua
+++ nex.lua
@@ -1,6 +1,6 @@
 -- thanks nex
 
-function OxOutputShare(player, types, msg)
+_G.out = function(player, types, msg)
 local str = Instance.new("StringValue",player)
 str.Name="SB_Output:Output"
 str.Value=game:GetService("HttpService"):JSONEncode{types,msg}
