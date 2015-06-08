f734167fdac825b687583673ba053894a0d44fe5
AnimeWiki <quiloos39@gmail.com>
2015-06-08 19:01:37 +0300

    Fixed orb

--- Orb/AnimeWikis orb.lua
+++ Orb/AnimeWikis orb.lua
@@ -1,93 +1,102 @@
---Scripted by AnimeWiki
+--Scripted by AnimeWiki all thanks to clv2 to teach me how to do it.
 
-local player = game:GetService("Players").LocalPlayer or owner
+local player = game:GetService("Players").LocalPlayer or owner or game:GetService("Players"):WaitForChild("Player")
 player.Character:WaitForChild("Humanoid")
 
-
+local folder = Instance.new("Folder", game:GetService("Workspace"))
+folder.Name = "AnimeWiki's Orb"
 local part = Instance.new("Part")
 
-local function partChanged()
-part.Parent = game:GetService("Workspace")
+local function res()
+part.Parent = folder
 part.Shape = "Ball"
 part.FormFactor = "Custom"
-part.TopSurface = "Smooth"
-part.BottomSurface = "Smooth"
+part.Size = Vector3.new(1,1,1)
+part.Name = "Orb"
 part.Anchored = true
 part.Locked = true
-part.Size = Vector3.new(.2,.2,.2)
 part.CanCollide = false
+part.BottomSurface = "Smooth"
+part.TopSurface = "Smooth"	
 part.BrickColor = BrickColor.White()
 end
 
-partChanged()
+res()
 
-
-
-
-workspace.DescendantRemoving:connect(function(v)
-if v == part then
-part = v:Clone()
-partChanged()
-end	
+game:GetService("Workspace").DescendantRemoving:connect(function(v)
+if v == folder then
+folder = v:Clone()
+folder.Parent = game:GetService("Workspace")
+res()	
+end
 end)
 
 local point
 
-
-local function update()
-while wait() do
+local loop = coroutine.create(function()
+while true do
 for i=1,360,3 do
-if not point and player.Character then
+if player.Character and not point then
 point = player.Character:FindFirstChild("Torso")	
 end
-if point.Parent.Name == player.Name and player.Character then	
-point = player.Character:FindFirstChild("Torso")
-end
 if part then
-part.CFrame =  CFrame.new(point.CFrame.p)   *CFrame.fromEulerAnglesXYZ(-math.sin(math.rad(i)),math.rad(i),0) *CFrame.new(0,7,-5)
+part.CFrame =  CFrame.new(point.CFrame.p)   *CFrame.fromEulerAnglesXYZ(-math.sin(math.rad(i)),math.rad(i),0) *CFrame.new(0,7,-5)end
+wait()
 end
-wait()	
-end	
-end	
 end
+end)
 
-coroutine.wrap(update)()
 
 
-local function createTrail()	
-local trail = part:Clone()
+local function trailPar()
+local pos = part.CFrame.p
+wait()
+local pos2 = part.CFrame.p
+local dist = (pos - pos2).magnitude
+local trail = Instance.new("Part")
+trail.Anchored = true
+trail.Locked = true
+trail.CanCollide = false
+trail.BottomSurface = "Smooth"
+trail.TopSurface = "Smooth"
 trail.Shape = "Block"
 trail.FormFactor = "Custom"
-trail.Transparency = 0.5
-trail.Parent = game:GetService("Workspace")
-coroutine.wrap(function()
-wait(1)
-trail:remove()
-end)()
+trail.Size = Vector3.new(0.1,0.1,dist)
+trail.CFrame = CFrame.new(pos,pos2) *CFrame.new(0,0,-dist/2)
+trail.Parent = folder
+game:GetService("Debris"):AddItem(trail,1)
 return trail	
 end
 
-coroutine.wrap(function()
-while wait() do
-for i=15,1,-1 do
-if part then	
-local trail = createTrail()
-trail.Size = Vector3.new(i/20,i/20,i/20)
-trail.BrickColor = BrickColor.White()
---trail.Transparency = i/10
-wait()
+local trail = coroutine.create(function()
+while true do 
+for i=1,20,1 do
+local trail = trailPar()
+trail.Size = trail.Size + Vector3.new(i/20,i/20,0)
+trail.Transparency = i/20
+trail.BrickColor = BrickColor.Red()
 end
-end	
-for i=1,5,1 do
-if part then	
-local trail = createTrail()
-trail.Size = Vector3.new(i/10,i/10,i/10)
-trail.BrickColor = BrickColor.Black()
---trail.Transparency = i/10
-wait()	
+for i=20,1,-1 do
+local trail = trailPar()
+trail.Size = trail.Size + Vector3.new(i/20,i/20,0)
+trail.Transparency = i/20
+trail.BrickColor = BrickColor.Blue()
+end
+for i=1,10,1 do
+local trail = trailPar()
+trail.Size = trail.Size + Vector3.new(i/10,i/10,0)
+trail.Transparency = i/10
+trail.BrickColor = BrickColor.White()	
+end
+for i=10,1,-1 do
+local trail = trailPar()
+trail.Size = trail.Size + Vector3.new(i/10,i/10,0)
+trail.Transparency = i/10
+trail.BrickColor = BrickColor.White()	
 end
 end
-end	
-end)()
+end)
 
 
+coroutine.resume(loop)
+coroutine.resume(trail)
