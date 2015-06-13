cb0bf6bc25b1a62b93b748f651126a04cba393c1
Quiloos39 <quiloos39@gmail.com>
2015-06-13 15:03:59 +0300

    Delete nex.lua

--- nex.lua
+++
@@ -1,7 +0,0 @@
--- thanks nex
-
-_G.out = function(player, types, msg)
-local str = Instance.new("StringValue",player)
-str.Name="SB_Output:Output"
-str.Value=game:GetService("HttpService"):JSONEncode{types,msg}
-end
