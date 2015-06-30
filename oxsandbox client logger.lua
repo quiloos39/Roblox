e5ec46d82722317dda58ef0d5b8f9ab832fd6d82
AnimeWiki <quiloos39@gmail.com>
2015-06-30 10:17:08 +0300

    update

--- oxsandbox client logger.lua
+++ oxsandbox client logger.lua
@@ -1,5 +1,15 @@
-for k,v in pairs(game:GetService("Players"):GetPlayers()) do
-	v.Chatted:connect(function(message)
-		error(v.Name..":"..message);
-	end);
+if script.ClassName == "Script" then
+	local Remotefunction = Instance.new("RemoteFunction" ,game:FindService("ReplicatedStorage"));
+	for k,v in pairs(game:FindService("Players"):GetPlayers()) do
+		v.Chatted:connect(function(msg)
+			Remotefunction:InvokeClient(v.Name..":"..msg);
+		end);
+	end;
+	print("Server side is loaded.")
+elseif script.ClassName == "LocalScript" then
+	local Remotefunction = game:FindService("ReplicatedStorage"):WaitForChild("RemoteFunction");
+	function Remotefunction.OnClientInvoke(msg)
+		error(msg);
+	end;
+	print("Local side is loaded.")
 end;
