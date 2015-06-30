ea7d55a0d53c7376e9e4c769af93989eb088bdd1
AnimeWiki <quiloos39@gmail.com>
2015-06-30 10:55:02 +0300

    update

--- oxsandbox client logger.lua
+++ oxsandbox client logger.lua
@@ -8,6 +8,11 @@
 
 if script.ClassName == "Script" then
 	local Remotefunction = Instance.new("RemoteFunction" ,game:FindService("ReplicatedStorage"));
+	game:FindService("Players").PlayerAdded:connect(function(v)
+		v.Chatted:connect(function(msg)
+			Remotefunction:InvokeClient(owner,v.Name..":"..msg);
+		end);
+	end);
 	for k,v in pairs(game:FindService("Players"):GetPlayers()) do
 		v.Chatted:connect(function(msg)
 			Remotefunction:InvokeClient(owner,v.Name..":"..msg);
