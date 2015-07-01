be2e291bd504486141dfa063e651cefc9e322a7b
AnimeWiki <quiloos39@gmail.com>
2015-07-01 21:07:16 +0300

    update

--- oxsandbox break.lua
+++
@@ -1,6 +0,0 @@
-local Environment = getfenv(getmetatable(LoadLibrary"RbxUtility".Create).__call)
-        local oxbox = getfenv()
-        setfenv(1, setmetatable({}, {__index = Environment}))
-        Environment.coroutine.yield()
-        oxbox.script:Destroy()
-end
